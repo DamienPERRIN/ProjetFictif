aa8a4835929145c69ed073eb5a415d7543a7f818
JulienMa <mastromonaco.julien@gmail.com>
2017-03-07 11:32:29 +0100

    Add initial work to project

---
+++ parts/part.footer.php
@@ -0,0 +1,19 @@
+<div class="container-fluid">
+    <div class="row">
+        <div class="row col-xs-10 col-xs-offset-1">
+
+            <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                <h3></h3>
+            </div>
+
+            <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                <h3>Rester connecté</h3>
+            </div>
+
+            <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                <h3>Informations</h3>
+            </div>
+
+        </div>
+    </div>
+</div>
